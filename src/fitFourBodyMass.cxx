734030a6d22e31da12c5ee86d3dd79df9a86882c
James Robinson <james.em.robinson@gmail.com>
2017-06-16 13:32:05 +0200

    Version for talk on 2017-06-16

--- src/fitFourBodyMass.cxx
+++ src/fitFourBodyMass.cxx
@@ -10,6 +10,7 @@
 #include "TFile.h"
 #include "TLegend.h"
 #include "TMath.h"
+#include "TStyle.h"
 // RooFit
 #include "RooAddPdf.h"
 #include "RooArgList.h"
@@ -34,18 +35,19 @@
 int main(int /*argc*/, char** /*argv*/) {  
   using namespace SpuriousSignal;
   
-  int MASS_STEP = 20;
-  int PRINT_LEVEL = 1;
+  int MASS_STEP = 1000; //20;
+  int PRINT_LEVEL = -1;
     
   // Disable RooFit and ROOT messages
   RooMsgService::instance().setGlobalKillBelow(RooFit::FATAL);
   gErrorIgnoreLevel = kBreak;
+  gStyle->SetOptTitle(0);
   
   // Setup colours and mass categories
   std::vector<int> colours = {kViolet, kGreen + 3, kBlue, kRed};
-  // std::map< std::string, std::pair<int, int> > mass_categories = {{"low", std::make_pair<int, int>(245, 485)},
-  //                                                                 {"high", std::make_pair<int, int>(335, 1140)}};
-  std::map< std::string, std::pair<int, int> > mass_categories = {{"low", std::make_pair<int, int>(245, 485)}};
+  std::map< std::string, std::pair<int, int> > mass_categories = {{"low", std::make_pair<int, int>(245, 485)},
+                                                                  {"high", std::make_pair<int, int>(335, 1140)}};
+  // std::map< std::string, std::pair<int, int> > mass_categories = {{"low", std::make_pair<int, int>(245, 485)}};
 
   TFile f_inputWS("/afs/cern.ch/user/a/andari/public/Leo/SigParam_low_cbga_cut5/Parameterized/SM/res_SM_CBGA_Parameterized_workspace.root");
   RooWorkspace wk(*(RooWorkspace*)f_inputWS.Get("signalWS"));
@@ -71,11 +73,12 @@ int main(int /*argc*/, char** /*argv*/) {
 
   for (auto mass_kv : mass_categories) {
     // Define parameter ranges
-    double mass_dn((mass_kv.first == "low") ? 220 : 0);
-    double mass_up((mass_kv.first == "low") ? 320 : 1000);
+    double peak_min((mass_kv.first == "low") ? 260 : 0);
+    double peak_max((mass_kv.first == "low") ? 280 : 1000);
     
     // Define data parameters
     RooRealVar mass("mass", "m_{yyjj}", mass_kv.second.first, mass_kv.second.second);
+    // RooFormulaVar mass_scaled("mass_scaled", ("((mass / " + std::to_string(mass_kv.second.first) + ") . - 1)").c_str(), RooArgList(mass));
     RooRealVar weight("weight", "event weight", 1e-10, 1e10);
 
     // Get the data
@@ -87,8 +90,8 @@ int main(int /*argc*/, char** /*argv*/) {
     int mass_point = mass_kv.second.first - MASS_STEP;
     std::vector<double> mass_points((mass_kv.second.second - mass_kv.second.first) / MASS_STEP + 1); 
     std::generate(mass_points.begin(), mass_points.end(), [&mass_point, &MASS_STEP]{ return mass_point += MASS_STEP; }); 
-    MSG_INFO("Will perform fits for " << mass_points.size() << " mass points between " << mass_kv.second.first << " and " << mass_kv.second.second << " GeV");
-    
+    MSG_INFO("Will perform fits for \033[1m" << mass_points.size() << "\033[0m mass points in the range \033[1m" << mass_kv.second.first << " - " << mass_kv.second.second << "\033[0m GeV");
+
     // Signal: CB+Gaus
     RooRealVar mass_resonance("mass_resonance", "mass of resonance", 305);
     RooFormulaVar signal_gaus_mean("signal_gaus_mean", "a_muGANom_SM_c2 + b_muGANom_SM_c2 * (mass_resonance / 100. - 1) + c_muGANom_SM_c2 * (mass_resonance / 100. - 1) * (mass_resonance / 100. - 1) + mass_resonance", RooArgList(a_muGANom_SM_c2, b_muGANom_SM_c2, c_muGANom_SM_c2, mass_resonance));
@@ -103,7 +106,7 @@ int main(int /*argc*/, char** /*argv*/) {
     RooAddPdf signal_PDF("signal_PDFa", "CB + Gaussian", RooArgList(signal_CB, signal_gaus), signal_frac_CB);
     
     // Novosibirsk
-    RooRealVar novosibirsk_peak("novosibirsk_peak", "peak of Novosibirsk", 270, mass_dn, mass_up);
+    RooRealVar novosibirsk_peak("novosibirsk_peak", "peak of Novosibirsk", 270, peak_min, peak_max);
     RooRealVar novosibirsk_tail("novosibirsk_tail", "tail of Novosibirsk", -1, -20, 20);
     RooRealVar novosibirsk_width("novosibirsk_width", "width of Novosibirsk", 30, 0, 500);
     RooNovosibirsk novosibirsk("novosibirsk", "novosibirsk", mass, novosibirsk_peak, novosibirsk_width, novosibirsk_tail);
@@ -125,17 +128,22 @@ int main(int /*argc*/, char** /*argv*/) {
     // tail of Novosibirsk [-0.87915 +/- 11.6104]
     
     // Modified Gamma
-    RooRealVar gamma_Gamma("gamma_Gamma", "gamma of Gamma", 8, 0, 1000);
-    RooRealVar gamma_beta0("gamma_beta0", "beta0 of Gamma", 0.07, 0, 100);
-    RooRealVar gamma_beta1("gamma_beta1", "beta1 of Gamma", 0.01, -0.1, 0.1);
-    RooRealVar gamma_mu("gamma_mu", "mean of Gamma", 270, mass_dn, mass_up);
-    RooFormulaVar gamma_beta("gamma_beta", "@1 * exp(@2 * @0)", RooArgList(mass, gamma_beta0, gamma_beta1)); // exponential as we require beta > 0 everywhere
-    RooGamma modified_gamma("modified_gamma", "modified_gamma", mass, gamma_Gamma, gamma_beta, gamma_mu);
+    // RooRealVar gamma_gamma("gamma_gamma", "gamma of Gamma", (mass_kv.first == "low" ? 1.0 : 0.07), 0, 100);
+    RooRealVar gamma_gamma0("gamma_gamma0", "gamma0 of Gamma", (mass_kv.first == "low" ? 1.0 : 0.07), 0, 100);
+    RooRealVar gamma_gamma1("gamma_gamma1", "gamma1 of Gamma", 0.0, -0.01, 0.01);
+    RooFormulaVar gamma_gamma("gamma_gamma", ("gamma_gamma0 * exp(gamma_gamma1 * ((mass / " + std::to_string(mass_kv.second.first) + ") - 1.0))").c_str(), RooArgList(mass, gamma_gamma0, gamma_gamma1)); // exponential as we require beta > 0 everywhere
+    RooRealVar gamma_beta0("gamma_beta0", "beta0 of Gamma", (mass_kv.first == "low" ? 65 : 190), 0, 1000);
+    RooRealVar gamma_beta1("gamma_beta1", "beta1 of Gamma", 0.0, -0.01, 0.01);
+    RooFormulaVar gamma_beta("gamma_beta", ("gamma_beta0 * exp(gamma_beta1 * ((mass / " + std::to_string(mass_kv.second.first) + ") - 1.0))").c_str(), RooArgList(mass, gamma_beta0, gamma_beta1)); // exponential as we require beta > 0 everywhere
+    RooRealVar gamma_mu("gamma_mu", "mean of Gamma", 270, peak_min, peak_max);
+    RooGamma modified_gamma("modified_gamma", "modified_gamma", mass, gamma_gamma, gamma_beta, gamma_mu);
+
     // // Low 245
-    // gamma_Gamma	  = 8	 +/-  nan	(limited)
-    // gamma_beta0	  = 50	 +/-  nan	(limited)
-    // gamma_beta1	  = 0	 +/-  nan	(limited)
-    // gamma_mu	  = 270	 +/-  nan	(limited)
+    // gamma of Gamma [16.2801 +/- 1.51294e-05]
+    // mean of Gamma [220.003 +/- 0.000508199]
+    // beta0 of Gamma [2.11268 +/- 0.000124382]
+    // beta1 of Gamma [0.0999952 +/- 1.21529e-07]
+    // mass of resonance [245 +/- 0],
     // // High 335
     // gamma of Gamma [13.6365 +/- 0.0103797]
     // mean of Gamma [17.2259 +/- 165.527]
@@ -151,14 +159,15 @@ int main(int /*argc*/, char** /*argv*/) {
     // mean of Gamma [17.2259 +/- 0.123555]
     // beta0 of Gamma [0.111925 +/- 0.0031181]
     // beta1 of Gamma [0.054172 +/- 0.00171601]
-
         
     // Modified Cauchy
-    RooRealVar cauchy_mu("cauchy_mu", "location of Cauchy", 270, mass_dn, mass_up);
-    RooRealVar cauchy_s0("cauchy_s0", "s0 of Cauchy", 10, 0, 1000);
-    RooRealVar cauchy_s1("cauchy_s1", "s1 of Cauchy", 0.01, -0.1, 0.1);
-    RooFormulaVar cauchy_s("cauchy_s", "@1 * exp(@2 * @0)", RooArgList(mass, cauchy_s0, cauchy_s1)); // exponential as we require s > 0 everywhere
-    RooGenericPdf modified_cauchy("modified_cauchy", "modified_cauchy", "TMath::CauchyDist(@0, @1, @2)", RooArgList(mass, cauchy_mu, cauchy_s));
+    // RooRealVar cauchy_s1("cauchy_s1", "s1 of Cauchy", 0.01, -0.1, 0.1);
+    // RooFormulaVar cauchy_s("cauchy_s", "@1 * exp(@2 * @0)", RooArgList(mass, cauchy_s0, cauchy_s1)); // exponential as we require s > 0 everywhere
+    RooRealVar cauchy_mu("cauchy_mu", "location of Cauchy", 270, peak_min, peak_max);
+    RooRealVar cauchy_s0("cauchy_s0", "s0 of Cauchy", (mass_kv.first == "low" ? 200 : 150), 0, 10000);
+    RooRealVar cauchy_s1("cauchy_s1", "s1 of Cauchy",  (mass_kv.first == "low" ? 30.0 : 2.0), -1000, 1000);
+    RooFormulaVar cauchy_s("cauchy_s", ("cauchy_s0 * exp(cauchy_s1 * ((mass / " + std::to_string(mass_kv.second.first) + ") - 1.0))").c_str(), RooArgList(mass, cauchy_s0, cauchy_s1)); // exponential as we require beta > 0 everywhere
+    RooGenericPdf modified_cauchy("modified_cauchy", "modified_cauchy", "TMath::CauchyDist(mass, cauchy_mu, cauchy_s)", RooArgList(mass, cauchy_mu, cauchy_s));
     // // Low 245
     // cauchy_mu	  = 293.993	 +/-  1.69854	(limited)
     // cauchy_s0	  = 2.74147	 +/-  0.176684	(limited)
@@ -181,7 +190,7 @@ int main(int /*argc*/, char** /*argv*/) {
             
     // Number of signal and background events
     RooRealVar nSig("nSig", "number of signal events", 0, 10);
-    RooRealVar nBkg("nBkg", "number of background events", 0, 10000);
+    RooRealVar nBkg("nBkg", "number of background events", 1000, 10000);
 
     // Setup fit functions
     std::vector<RooAbsPdf*> bkg_functions;
@@ -192,8 +201,10 @@ int main(int /*argc*/, char** /*argv*/) {
     // Construct parameter sets that need to be remembered
     std::vector<ParameterSet> parameter_sets;
     parameter_sets.push_back(ParameterSet("Novosibirsk", {&novosibirsk_peak, &novosibirsk_width, &novosibirsk_tail, &nSig, &nBkg, &mass_resonance}));
-    parameter_sets.push_back(ParameterSet("Modified Gamma", {&gamma_Gamma, &gamma_mu, &gamma_beta0, &gamma_beta1, &nSig, &nBkg, &mass_resonance}));
+    parameter_sets.push_back(ParameterSet("Modified Gamma", {&gamma_gamma0, &gamma_gamma1, &gamma_mu, &gamma_beta0, &gamma_beta1, &nSig, &nBkg, &mass_resonance}));
     parameter_sets.push_back(ParameterSet("Modified Cauchy", {&cauchy_mu, &cauchy_s0, &cauchy_s1, &nSig, &nBkg, &mass_resonance}));
+    // parameter_sets.push_back(ParameterSet("Modified Gamma", {&gamma_gamma, &gamma_mu, &gamma_beta0, &gamma_beta1, &nSig, &nBkg, &mass_resonance}));
+    // RooLognormal (const char *name, const char *title, RooAbsReal &_x, RooAbsReal &_m0, RooAbsReal &_k)
 
     // Setup fit functions
     std::vector<RooAbsPdf*> splusb_functions;
@@ -211,7 +222,7 @@ int main(int /*argc*/, char** /*argv*/) {
     for (unsigned idx = 0; idx < bkg_functions.size(); ++idx) {
       // Selection criterion
       double Z_spurious(0), Z_spurious_max(-99);
-      MSG_INFO("Using fit function " << splusb_functions.at(idx)->getTitle());
+      MSG_INFO("Using \033[1m" << mass_kv.first << " mass \033[0m fit function \033[1m" << splusb_functions.at(idx)->getTitle() << "\033[0m");
 
       // Iterate over mass points
       for (int iMass = mass_kv.second.first; iMass < mass_kv.second.second; iMass += MASS_STEP) {
@@ -219,9 +230,9 @@ int main(int /*argc*/, char** /*argv*/) {
         fit_status = splusb_functions.at(idx)->fitTo(data, RooFit::SumW2Error(true), RooFit::NumCPU(4), RooFit::Minimizer("Minuit2", "simplex"), RooFit::Hesse(false), RooFit::Minos(false), RooFit::PrintLevel(PRINT_LEVEL), RooFit::Save(true))->status();
         if (fit_status != 0) { MSG_ERROR("... simplex fit did not converge: status = " << fit_status); }
         MSG_DEBUG("... simplex: " << parameter_sets.at(idx));
-        fit_status = splusb_functions.at(idx)->fitTo(data, RooFit::SumW2Error(true), RooFit::NumCPU(4), RooFit::Minimizer("Minuit2", "migradimproved"), RooFit::Hesse(true), RooFit::Minos(true), RooFit::PrintLevel(PRINT_LEVEL), RooFit::Save(true))->status();
-        if (fit_status != 0) { MSG_ERROR("... migrad + improve + hesse + minos fit did not converge: status = " << fit_status); }
-        MSG_DEBUG("... migrad+improve+hesse+minos: " << parameter_sets.at(idx));
+        fit_status = splusb_functions.at(idx)->fitTo(data, RooFit::SumW2Error(true), RooFit::NumCPU(4), RooFit::Minimizer("Minuit2", "migradimproved"), RooFit::Hesse(true), RooFit::Minos(false), RooFit::PrintLevel(PRINT_LEVEL), RooFit::Save(true))->status();
+        if (fit_status != 0) { MSG_ERROR("... migrad + improve + hesse fit did not converge: status = " << fit_status); }
+        MSG_INFO("... migrad + improve + hesse: " << parameter_sets.at(idx));
         // Record these parameters if Z > max(Z) so far
         Z_spurious = nSig.getVal() / nSig.getError(); // (N_S +/- errS_SumW2_true) / (errS_SumW2_false)
         MSG_INFO("... resonance mass = " << mass_resonance.getVal() << " => nSig: " << nSig.getVal() << " +/- " << nSig.getError() << ", nBkg: " << nBkg.getVal() << " +/- " << nBkg.getError() << " [" << fit_status << "] => Z = " << Z_spurious);
@@ -257,10 +268,11 @@ int main(int /*argc*/, char** /*argv*/) {
     }
     // for (int i = 0; i < frame->numItems(); ++i) { MSG_INFO(frame->getObject(i)->GetName()); }
     frame->Draw();
+    frame->Draw();
     TLegend legend(0.6, 0.7, 0.9, 0.9);
     legend.AddEntry(frame->findObject("signal_plus_novosibirsk_Norm[mass]"), "Novosibirsk", "L");
-    legend.AddEntry(frame->findObject("signal_plus_modified_gamma_Norm[mass]"), "Gamma", "L");
-    legend.AddEntry(frame->findObject("signal_plus_modified_cauchy_Norm[mass]"), "Cauchy", "L");
+    legend.AddEntry(frame->findObject("signal_plus_modified_gamma_Norm[mass]"), "Modified Gamma", "L");
+    legend.AddEntry(frame->findObject("signal_plus_modified_cauchy_Norm[mass]"), "Modified Cauchy", "L");
     legend.Draw();    
     canvas.Print((std::string("output/m_yyjj_") + mass_kv.first + "Mass.pdf").c_str());
     MSG_INFO("Saved " + mass_kv.first + " mass plot");
