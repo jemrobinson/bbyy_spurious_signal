1693b3d5efc09b46ab5a9798f67c9b0fbb03a6b5
James Robinson <james.em.robinson@gmail.com>
2017-12-12 18:54:50 +0100

    Rewrote pseudodata generation to cope better with negative weights

--- python/generate_pseudodata.py
+++ python/generate_pseudodata.py
@@ -22,14 +22,14 @@ logging.basicConfig(stream=sys.stdout, format="%(asctime)s %(levelname)8s: %(mes
 logger.setLevel(logging.INFO)
 
 # Input settings
-version_directory = "20171116"
+version_directory = "20171212"
 expected_hh_limits_pb = {260: 1.15, 275: 1.0, 300: 0.9, 325: 0.8, 350: 0.7, 400: 0.55, 450: 0.5, 500: 0.38,  750: 0.18,  1000: 0.13}
-masses = [260, 275, 300, 400, 750, None]
+masses = [260, 275, 300, 325, 350, 400, 450, 500, 750, 1000, None]
 mass_categories = ["high", "low"]
 tag_categories = ["1", "2"]
 signal_hypos = [1, 1.5, 2]
 nPseudodataSamples = 1000
-np.random.seed(20170711)
+np.random.seed(20171212)
 
 # Create pseudodata
 for idx_mass, mass in enumerate(masses, start=1):
@@ -41,31 +41,48 @@ for idx_mass, mass in enumerate(masses, start=1):
         total_nSig = dict((m, dict((t, []) for t in tag_categories)) for m in mass_categories)
         signal_hypo_dir = ensure_path(mass_dir, "signal_hypothesis_{}".format(idx_signal_hypo))
         logger.info("... signal hypothesis {}/{}".format(idx_signal_hypo + 1, len(signal_hypos)))
-        for idx_sample in tqdm.trange(1, nPseudodataSamples + 1):
-            sample_dir = ensure_path(signal_hypo_dir, "sample_{}".format(idx_sample))
-            for mass_category in mass_categories:
-                for tag_category in tag_categories:
+        # Iterate over tag and mass categories for a given signal hypothesis
+        sum_weights_bkg, sum_weights_sig = {}, {}
+        for mass_category in mass_categories:
+            for tag_category in tag_categories:
+                # Load background events
+                masses_bkg, weights_bkg = [], []
+                with open(os.path.join("input", "m_yyjj_SM_bkg_{}Mass_{}tag_tightIsolated_positive_weights.csv".format(mass_category, tag_category)), "rb") as f_input:
+                    for row in csv.reader(f_input, delimiter="\t"):
+                        masses_bkg.append(float(row[0]))
+                        weights_bkg.append(float(row[1]))
+                sum_weights_bkg[(mass_category, tag_category)] = sum(weights_bkg)
+                logger.debug("... => {} mass {}-tag pseudodata has {} background entries, corresponding to {:.2f} events".format(mass_category, tag_category, len(masses_bkg), sum_weights_bkg[(mass_category, tag_category)]))
+                nEventsPoisson_bkg = np.random.poisson(sum_weights_bkg[(mass_category, tag_category)], nPseudodataSamples)
+                normalised_weights_bkg = [_w / sum_weights_bkg[(mass_category, tag_category)] for _w in weights_bkg]
+                # Load signal events
+                masses_sig, weights_sig = [], []
+                if mass is not None:
+                    hh_xs_pb = (expected_hh_limits_pb[mass]) * signal_hypo
+                    weight_scale = float(hh_xs_pb) / 5. # default weights are for a 5 pb hh cross-section
+                    with open(os.path.join("input", "m_yyjj_Xhh_m{}_{}Mass_{}tag_tightIsolated_positive_weights.csv".format(mass, mass_category, tag_category)), "rb") as f_input:
+                        for row in csv.reader(f_input, delimiter="\t"):
+                            masses_sig.append(float(row[0]))
+                            weights_sig.append(float(row[1]))
+                sum_weights_sig[(mass_category, tag_category)] = sum(weights_sig)
+                logger.debug("... => {} mass {}-tag pseudodata has {} signal entries, corresponding to {:.2f} events".format(mass_category, tag_category, len(masses_sig), sum_weights_sig[(mass_category, tag_category)]))
+                nEventsPoisson_sig = np.random.poisson(sum_weights_sig[(mass_category, tag_category)], nPseudodataSamples)
+                normalised_weights_sig = [_w / sum_weights_sig[(mass_category, tag_category)] for _w in weights_sig]
+                # Write out pseudodata
+                for idx_sample in tqdm.trange(1, nPseudodataSamples + 1):
+                    sample_dir = ensure_path(signal_hypo_dir, "sample_{}".format(idx_sample))
                     f_output_name = os.path.join(sample_dir, "pseudodata_myyjj_{}_sample_{}_{}Mass_{}tag.txt".format(output_dir, idx_sample, mass_category, tag_category))
                     if os.path.isfile(f_output_name):
                         logger.warning("File {} exists! Skipping".format(f_output_name))
                         continue
                     with open(f_output_name, "wb") as f_output:
-                        nBkg, nSig = 0, 0
-                        # Draw events from background
-                        with open(os.path.join("input", "m_yyjj_SM_bkg_{}Mass_{}tag_tightIsolated.csv".format(mass_category, tag_category)), "rb") as f_input:
-                            for row in csv.reader(f_input, delimiter="\t"):
-                                if np.random.random() < float(row[1]):
-                                    f_output.write(row[0] + "\n")
-                                    nBkg += 1
-                        # Draw events from signal
+                        nBkg, nSig = nEventsPoisson_bkg[idx_sample - 1], nEventsPoisson_sig[idx_sample - 1]
+                        for _myyjj in np.random.choice(masses_bkg, nBkg, replace=False, p=normalised_weights_bkg):
+                            f_output.write("{}\n".format(_myyjj))
                         if mass is not None:
-                            hh_xs_pb = (expected_hh_limits_pb[mass]) * signal_hypo
-                            weight_scale = float(hh_xs_pb) / 5. # default weights are for a 5 pb hh cross-section
-                            with open(os.path.join("input", "m_yyjj_Xhh_m{}_{}Mass_{}tag_tightIsolated.csv".format(mass, mass_category, tag_category)), "rb") as f_input:
-                                for row in csv.reader(f_input, delimiter="\t"):
-                                    if np.random.random() < (float(row[1]) * weight_scale):
-                                        f_output.write(row[0] + "\n")
-                                        nSig += 1
+                            for _myyjj in np.random.choice(masses_sig, nBkg, replace=False, p=normalised_weights_sig):
+                                f_output.write("{}\n".format(_myyjj))
+                        for _myyjj in events_bkg + events_sig:
                         logger.debug("... => {} mass {}-tag pseudodata with {} background events and {} signal events".format(mass_category, tag_category, nBkg, nSig))
                         total_nBkg[mass_category][tag_category].append(nBkg)
                         total_nSig[mass_category][tag_category].append(nSig)
@@ -73,4 +90,4 @@ for idx_mass, mass in enumerate(masses, start=1):
             for tag_category in tag_categories:
                 nBkg = np.mean(total_nBkg[mass_category][tag_category])
                 nSig = np.mean(total_nSig[mass_category][tag_category])
-                logger.info("... overall average {} mass {}-tag pseudodata is {} background events and {} signal events".format(mass_category, tag_category, nBkg, nSig))
+                logger.info("... overall average {} mass {}-tag pseudodata is {:.3f} background events (expected {:.3f}) and {:.3f} signal events (expected {:.3f})".format(mass_category, tag_category, nBkg, sum_weights_bkg[(mass_category, tag_category)], nSig, sum_weights_sig[(mass_category, tag_category)]))
--- python/prepare_inputs.py
+++ python/prepare_inputs.py
@@ -12,23 +12,40 @@ for mass_category in ["low", "high"]:
     for tag_category in [0, 1, 2]:
         print "Now considering {} mass, {}-tag category...".format(mass_category, tag_category)
         with open(os.path.join("input", "m_yyjj_SM_bkg_{}Mass_{}tag_tightIsolated.csv".format(mass_category, tag_category)), "wb") as f_output:
-            nTotal, sumWTotal = 0, 0.0
-            for bkg in ["SM_bkg_yy", "SM_bkg_yj", "SM_bkg_jy", "SM_bkg_jj", "ggH", "ttH", "VBFH", "WH", "ZH", "bbH"]:
-                nBkg, sumWBkg = 0, 0.0
-                with open(os.path.join(input_directory, "m_yyjj_{}_{}Mass_{}tag_tightIsolated.csv".format(bkg, mass_category, tag_category)), "rb") as f_input:
-                    for row in csv.reader(f_input, delimiter="\t"):
-                        output_weight = float(row[1])
-                        f_output.write("\t".join((row[0], "{0:.20f}".format(output_weight))) + "\n")
-                        nBkg += 1
-                        sumWBkg += output_weight
-                print ".... {} had {} events, equivalent to {} in data".format(bkg, nBkg, sumWBkg)
-                nTotal += nBkg
-                sumWTotal += sumWBkg
+            with open(os.path.join("input", "m_yyjj_SM_bkg_{}Mass_{}tag_tightIsolated_positive_weights.csv".format(mass_category, tag_category)), "wb") as f_output_positive_weights:
+                nTotal, sumWTotal = 0, 0.0
+                for bkg in ["SM_bkg_yy", "SM_bkg_yj", "SM_bkg_jy", "SM_bkg_jj", "ggH", "ttH", "VBFH", "WH", "ZH", "bbH"]:
+                    _masses, _weights = [], []
+                    with open(os.path.join(input_directory, "m_yyjj_{}_{}Mass_{}tag_tightIsolated.csv".format(bkg, mass_category, tag_category)), "rb") as f_input:
+                        for row in csv.reader(f_input, delimiter="\t"):
+                            _masses.append(row[0])
+                            _weights.append(float(row[1]))
+                    nBkg, sum_weights = len(_weights), sum(_weights)
+                    sum_positive_weights = sum([_w for _w in _weights if _w > 0.])
+                    _scale_factor = sum_weights / sum_positive_weights if sum_weights > 0.0 else 0.0
+                    for _mass, _weight in zip(_masses, _weights):
+                        f_output.write("\t".join([_mass, "{0:.20f}".format(_weight)]) + "\n")
+                        if _weight > 0.0:
+                            f_output_positive_weights.write("\t".join([_mass, "{0:.20f}".format(_weight * _scale_factor)]) + "\n")
+                    print ".... {} had {} events, equivalent to {:.2f} in data ({:.2f} if negative weights are ignored): scale factor of {:.2f} was applied".format(bkg, nBkg, sum_weights, sum_positive_weights, _scale_factor)
+                    nTotal += nBkg
+                    sumWTotal += sum_weights
         print "=> Summary: {} events, equivalent to {} in data".format(nTotal, sumWTotal)
 
 # BSM Higgs
 for csv_file in glob.glob(os.path.join(input_directory, "m_yyjj_Xhh_m*.csv")):
+    with open(csv_file, "rb") as f_input:
+        _masses, _weights = [], []
+        for row in csv.reader(f_input, delimiter="\t"):
+            _masses.append(row[0])
+            _weights.append(float(row[1]))
+    sum_weights = sum(_weights)
+    sum_positive_weights = sum([_w for _w in _weights if _w > 0.])
+    _scale_factor = sum_weights / sum_positive_weights if sum_weights > 0.0 else 0.0
+    # Write outputs
     with open(os.path.join("input", os.path.basename(csv_file)), "wb") as f_output:
-        with open(csv_file, "rb") as f_input:
-            for row in csv.reader(f_input, delimiter="\t"):
-                f_output.write("\t".join((row[0], "{0:.20f}".format(float(row[1])))) + "\n")
+        with open(os.path.join("input", os.path.basename(csv_file)).replace(".csv", "_positive_weights.csv"), "wb") as f_output_positive_weights:
+            for _mass, _weight in zip(_masses, _weights):
+                f_output.write("\t".join([_mass, "{0:.20f}".format(_weight)]) + "\n")
+                if _weight > 0.0:
+                    f_output_positive_weights.write("\t".join([_mass, "{0:.20f}".format(_weight * _scale_factor)]) + "\n")
